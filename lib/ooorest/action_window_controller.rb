ac7fd5aee8277a08f17b75c218254f4323f36d1f
Raphaël Valyi <rvalyi@gmail.com>
2013-05-27 03:03:57 -0300

    not all Rails apps will use our Rest controller...

--- lib/ooorest/action_window_controller.rb
+++ lib/ooorest/action_window_controller.rb
@@ -3,7 +3,8 @@ require 'active_support/dependencies/autoload'
 require 'ooor'
 require 'ooor/base'
 
-require "action_controller"
+require 'action_controller'
+require 'ooorest/request_helper'
 
 module Ooorest
   class ModelNotFound < ::StandardError
@@ -13,6 +14,7 @@ module Ooorest
   end
 
   module ActionWindowController
+    include RequestHelper
 
     # GET /res_partners
     # GET /res_partners.json
@@ -153,64 +155,6 @@ module Ooorest
     end
 
     private
-      
-    def _authenticate!
-      instance_eval &Ooorest.authenticate_with
-    end
-
-    def _authorize!
-      instance_eval &Ooorest.authorize_with
-    end
-
-    def _audit!
-      instance_eval &Ooorest.audit_with
-    end
-
-    def _current_user
-      instance_eval &Ooorest.current_user_method
-    end
-
-#    private
-
-    def get_model_meta
-      get_model
-    end
-
-    def to_model_name(param)
-      @oe_model_name = param.gsub('-', '.')
-      @connection.class_name_from_model_key(@oe_model_name)
-    end
-
-    def context
-      @context ||= params.dup().tap do |c|
-        c[:active_id] = c[:id]
-        %w[model_name id _method controller action format _].each {|k| c.delete(k)}
-      end
-    end
-
-    def get_session_credentials
-      {ooor_user_id: 1, ooor_password: 'admin2', ooor_database: 'rails2'} #TODO deal with current_user
-    end
-
-    def get_connection
-      session_credentials = get_session_credentials
-      session_credentials.merge(params.slice(:ooor_user_id, :ooor_username, :ooor_password, :ooor_database)) #TODO dangerous?
-      Ooor::Connection.retrieve_connection(session_credentials)
-    end
-
-    def get_model
-      @connection ||= get_connection
-      @model_path = params[:model_name]
-      @model_name = to_model_name(params[:model_name])
-      raise Ooorest::ModelNotFound unless (@abstract_model = @connection.const_get(@oe_model_name))
-    end
-
-    def get_object
-#       if params[:id].index(",")
-#         @ids = params[:id].gsub('[', '').gsub(']', '').split(',').map {|i| i.to_i}
-#       end
-      raise Ooorest::ObjectNotFound unless (@object = @abstract_model.find(params[:id], fields: @fields && @fields.keys, context: context)) #TODO support multiple ids
-    end
 
     def _authenticate!
       instance_eval &Ooorest.authenticate_with
---
+++ lib/ooorest/request_helper.rb
@@ -0,0 +1,43 @@
+module Ooorest
+  module RequestHelper
+    def get_model_meta
+      get_model
+    end
+
+    def to_model_name(param)
+      @oe_model_name = param.gsub('-', '.')
+      @connection.class_name_from_model_key(@oe_model_name)
+    end
+
+    def context
+      @context ||= params.dup().tap do |c|
+        c[:active_id] = c[:id]
+        %w[model_name id _method controller action format _].each {|k| c.delete(k)}
+      end
+    end
+
+    def get_session_credentials
+      {ooor_user_id: 1, ooor_password: 'admin2', ooor_database: 'rails2'} #TODO deal with current_user
+    end
+
+    def get_connection
+      session_credentials = get_session_credentials
+      session_credentials.merge(params.slice(:ooor_user_id, :ooor_username, :ooor_password, :ooor_database)) #TODO dangerous?
+      Ooor::Connection.retrieve_connection(session_credentials)
+    end
+
+    def get_model
+      @connection ||= get_connection
+      @model_path = params[:model_name]
+      @model_name = to_model_name(params[:model_name])
+      raise Ooorest::ModelNotFound unless (@abstract_model = @connection.const_get(@oe_model_name))
+    end
+
+    def get_object
+#       if params[:id].index(",")
+#         @ids = params[:id].gsub('[', '').gsub(']', '').split(',').map {|i| i.to_i}
+#       end
+      raise Ooorest::ObjectNotFound unless (@object = @abstract_model.find(params[:id], fields: @fields && @fields.keys, context: context)) #TODO support multiple ids
+    end
+  end
+end
