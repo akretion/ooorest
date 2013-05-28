d1e1e294dee27d6e494e133fc6687b3a54d7775d
Raphaël Valyi <rvalyi@gmail.com>
2013-05-28 02:01:30 -0300

    refactored connection method

--- lib/ooorest/request_helper.rb
+++ lib/ooorest/request_helper.rb
@@ -6,7 +6,7 @@ module Ooorest
 
     def to_model_name(param)
       @oe_model_name = param.gsub('-', '.')
-      @connection.class_name_from_model_key(@oe_model_name)
+      connection.class_name_from_model_key(@oe_model_name)
     end
 
     def context
@@ -20,17 +20,20 @@ module Ooorest
       {ooor_user_id: 1, ooor_password: 'admin2', ooor_database: 'rails2'} #TODO deal with current_user
     end
 
-    def get_connection
-      session_credentials = get_session_credentials
-      session_credentials.merge(params.slice(:ooor_user_id, :ooor_username, :ooor_password, :ooor_database)) #TODO dangerous?
-      Ooor::Connection.retrieve_connection(session_credentials)
+    def connection
+      if @connection
+        @connection
+      else
+        session_credentials = get_session_credentials
+        session_credentials.merge(params.slice(:ooor_user_id, :ooor_username, :ooor_password, :ooor_database)) #TODO dangerous?
+        @connection = Ooor::Connection.retrieve_connection(session_credentials)
+      end
     end
 
     def get_model
-      @connection ||= get_connection
       @model_path = params[:model_name]
       @model_name = to_model_name(params[:model_name])
-      raise Ooorest::ModelNotFound unless (@abstract_model = @connection.const_get(@oe_model_name))
+      raise Ooorest::ModelNotFound unless (@abstract_model = connection.const_get(@oe_model_name))
     end
 
     def get_object
