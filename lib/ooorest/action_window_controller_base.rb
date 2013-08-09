8ec719d075fbc088d9f18210e5c63b9f07a6e083
Raphaël Valyi <rvalyi@gmail.com>
2013-08-09 22:56:57 +0000

    cleanup

--- lib/ooorest/action_window_controller_base.rb
+++ lib/ooorest/action_window_controller_base.rb
@@ -5,6 +5,7 @@ require 'action_pack'
 module Ooorest
   class ActionWindowControllerBase < ActionController::Base
     include ActionWindowController
+    respond_to :html, :json, :xml
 
     before_filter :ooor_model_meta, :except => [:dashboard]
     before_filter :ooor_object, :only => [:show, :edit, :delete, :update, :show_in_app]
